51d9774c3e09fe400ae8869d4d76a6949717b24c
Jacob Minshall <minshallj@fingertiptech.com>
2014-12-10 22:34:47 -0800

    keeping track of memory from the stack pops to free

--- harness.c
+++ harness.c
@@ -1,18 +1,21 @@
 #include "stack.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int test_stack()
 {
 	Stack s;
-	int i;
+	int i, *tmp;
 
 	stack_init(&s, sizeof(int));
 
 	for (i = 0; i < 10; i++)
 		stack_push(&s, (void *)&i);
 
-	for (i = 0; i < 10; i++)
-		printf("%d,", *(int *)stack_pop(&s));
+	for (i = 0; i < 10; i++) {
+		printf("%d,", *(tmp = (int *)stack_pop(&s)));
+		free(tmp);
+	}
 
 	return 0;
 }
