a24072a06c2e689f7383d90f87d948335f4c7a89
Jacob Minshall <minshallj@fingertiptech.com>
2014-12-14 11:24:51 -0800

    testing: linked_list: working tests for linked list
    Broken tests are commented out, when removing a segfault happens during
    the add part, this might be some compiler optimization that is
    happening.

--- Makefile
+++ Makefile
@@ -3,14 +3,17 @@ CFLAGS=-c
 
 default: test
 
-harness.o: harness.c stack.h
+harness.o: harness.c stack.h linked_list.h
 	$(CC) $(CFLAGS) harness.c
 
 stack.o: stack.c stack.h
 	$(CC) $(CFLAGS) stack.c
 
-test: harness.o stack.o
-	$(CC) harness.o stack.o -o test.out
+linked_list.o: linked_list.c linked_list.h
+	$(CC) $(CFLAGS) linked_list.c
+
+test: harness.o stack.o linked_list.o
+	$(CC) harness.o stack.o linked_list.o -o test.out
 
 clean:
-	rm harness.o stack.o test.out
+	rm harness.o stack.o linked_list.o test.out
--- harness.c
+++ harness.c
@@ -1,4 +1,5 @@
 #include "stack.h"
+#include "linked_list.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,6 +8,7 @@ int test_stack()
 	Stack s;
 	int i, *tmp;
 
+	printf("\n\ttest stack starting\n");
 	stack_init(&s, sizeof(int));
 
 	for (i = 0; i < 10; i++)
@@ -16,6 +18,50 @@ int test_stack()
 		printf("%d,", *(tmp = (int *)stack_pop(&s)));
 		free(tmp);
 	}
+	printf("\n\ttest stack finished\n");
+
+	return 0;
+}
+
+int test_list(int prepend)
+{
+	LinkedList l;
+	int i, *tmp;
+
+	printf("\n\ttest list %s starting\n", prepend ? "prepend" : "append");
+	linked_list_init(&l);
+
+	for (i = 0; i < 10; i++)
+		if (prepend)
+			linked_list_prepend(&l, &i, sizeof(int));
+		else
+			linked_list_append(&l, &i, sizeof(int));
+
+	for (i = 0; i < 10; i++) {
+		tmp = linked_list_index(&l, i+1);
+		if (tmp != NULL) {
+			printf("%d,", *tmp);
+			free(tmp);
+		} else {
+			printf("\tGot null on %d\n", i);
+		}
+	}
+	/*
+	for (i = 0; i < 5; i++)
+		linked_list_remove_index(&l, 1);
+	printf("\njust removed everything\n");
+
+	for (i = 0; i < 10; i++) {
+		tmp = linked_list_index(&l, i+1);
+		if (tmp != NULL) {
+			printf("%d,", *tmp);
+			free(tmp);
+		} else {
+			printf("\tGot null on %d\n", i);
+		}
+	}
+	*/
+	printf("\n\ttest list finished\n");
 
 	return 0;
 }
@@ -23,4 +69,6 @@ int test_stack()
 int main()
 {
 	test_stack();
+	test_list(1);
+	test_list(0);
 }
